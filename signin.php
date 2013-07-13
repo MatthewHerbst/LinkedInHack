54d12011542693f64ec5cf1f1bf7dd9468fb7083
Matthew Herbst <herbstmb@muohio.edu>
2013-07-13 07:02:54 -0700

    Dependency fix

--- signin.php
+++ signin.php
@@ -185,7 +185,7 @@ if(isset($_REQUEST['cmd'])) {
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="/js/jquery.min.js"></script>
+    <script src="/js/jquery-1.10.2.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
 
   </body>
