782eccc8a0e4fc7748f85221ab9ce82f172e21db
Matthew Herbst <herbstmb@muohio.edu>
2013-07-12 21:02:31 -0700

    test

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta name="author" content="Matthew Herbst">
+	<meta charset="utf-8">
+	<title>Our cool title!</title>
+	<link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+<body>
+</body>
+</html>
