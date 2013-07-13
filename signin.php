23d559734bab05bf7cfbe91905902d987a3b5cc2
Matthew Herbst <herbstmb@muohio.edu>
2013-07-13 07:00:51 -0700

    Dependency fix

--- signin.php
+++ signin.php
@@ -174,7 +174,7 @@ if(isset($_REQUEST['cmd'])) {
 	<?php
 		else:
 	?>
-	<?php header("Location: " + $goingTo . ".php"); ?>
+	<?php header("Location: http://www.174.34.170.64/selection.php"); ?>
 	<!--<script type="text/javascript">
 		//Send the user to the page they wanted
 		window.location = "http://www.htmlcodes.me/"
@@ -186,18 +186,7 @@ if(isset($_REQUEST['cmd'])) {
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="/js/jquery.js"></script>
-    <script src="/js/bootstrap-transition.js"></script>
-    <script src="/js/bootstrap-alert.js"></script>
-    <script src="/js/bootstrap-modal.js"></script>
-    <script src="/js/bootstrap-dropdown.js"></script>
-    <script src="/js/bootstrap-scrollspy.js"></script>
-    <script src="/js/bootstrap-tab.js"></script>
-    <script src="/js/bootstrap-tooltip.js"></script>
-    <script src="/js/bootstrap-popover.js"></script>
-    <script src="/js/bootstrap-button.js"></script>
-    <script src="/js/bootstrap-collapse.js"></script>
-    <script src="/js/bootstrap-carousel.js"></script>
-    <script src="/js/bootstrap-typeahead.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
 
   </body>
 </html>
