f2b610bafc70fd4951b1b2416d40beeed57cb280
Ankit Agarwal <ankit.agarwal@ankagarwal-ltm.local>
2013-07-13 09:23:38 -0700

    test

--- process.php
+++ process.php
@@ -92,7 +92,7 @@ $final_file = "curl -s http://174.34.170.64/temp/$id > /tmp/wingscript; sudo bas
 			</br>
 				<p>Please run this command in your terminal</p>
 			</br>
-				<pre class="curl-tag code">
+				<pre class="curl-tag">
 					<?php
 						echo "<p text-align='center'>" . $final_file . "</p>";
 					?>
