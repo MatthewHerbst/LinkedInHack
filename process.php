cdbf03195f53fcf01d1261107c3e006cc0e1f12f
Kartik Talwar <hi@kartikt.com>
2013-07-13 11:27:03 +0000

    Modified process

--- process.php
+++ process.php
@@ -2,16 +2,26 @@
 
 
 $data = $_GET['packages'];
+$id   = $_GET['id'];
 
+$manifest = '';
 
 foreach($data as $package) {
 
-    if(file_exists('./packages.'.$package)) {
-        print_r($package);
+    if(file_exists('./packages/')) {
+	$read_file = file_get_contents('./packages/'.$package.'.pp');
+	$manifest .= $read_file."\n\n";
     }
 
 }
 
+
+$puppet_file = './'.$id.'.pp';
+
+file_put_contents($puppet_file, $manifest);
+
+
+print_r($puppet_file);
 
 
 
