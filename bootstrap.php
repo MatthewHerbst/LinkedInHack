208de1426176537351d1f31a11183fd899cb9d60
Ankit Agarwal <ankit.agarwal@ankagarwal-ltm.local>
2013-07-13 05:51:27 -0700

    test

--- bootstrap.php
+++ bootstrap.php
@@ -35,7 +35,7 @@
 		<!-- One row with 3 columns, each taking the width of 4 items (a row can have up to 12 columns) -->
 		<div class="row">
 			<div class="span5">
-				<a href='/Windowsows.php'> <img src="/img/windows2.png" alt="Windows" width="300" height="300"> </a>
+				<a href='/Windows.php'> <img src="/img/windows2.png" alt="Windows" width="300" height="300"> </a>
 			</div>
 			<div class="span4">
 				<a href='/apple.php'> <img src="/img/apple.gif" alt="Mac" width="225" height="225">
