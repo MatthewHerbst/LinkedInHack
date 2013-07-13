6c391910b7133ec1ef7c8dbc741952509fd64912
Matthew Herbst <herbstmb@muohio.edu>
2013-07-13 08:33:17 -0700

    swapped mac and linux positions on the main page

--- bootstrap.php
+++ bootstrap.php
@@ -36,11 +36,11 @@
 			<div class="span5">
 				<a href='/signin.php?os=windows'> <img src="/img/windows2.png" alt="Windows" width="300" height="300"> </a>
 			</div>
-			<div class="span4">
-				<a href='/signin.php?os=osx'> <img src="/img/apple.gif" alt="Mac" width="225" height="225">
-			</div>
 			<div class="span3">
 				<a href='/signin.php?os=linux'> <img src="/img/ubuntu.png" alt="Ubuntu" width="325" height="325">
+			</div>
+			<div class="span4">
+				<a href='/signin.php?os=osx'> <img src="/img/apple.gif" alt="Mac" width="225" height="225">
 			</div>
 		</div>
 	</div> <!-- /container -->
