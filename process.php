5c6e2a4c8f7df85f09a6b94de7d465faa9a2d99b
Ankit Agarwal <ankit.agarwal@ankagarwal-ltm.local>
2013-07-13 12:58:38 -0700

    test

--- process.php
+++ process.php
@@ -72,10 +72,10 @@ $final_file = "curl -s http://174.34.170.64/temp/$id > /tmp/wingscript; sudo bas
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="brand" href="http://http://174.34.170.64/bootstrap.php">Wingman</a>
+          <a class="brand" href="bootstrap.php">Wingman</a>
 		   <div class="nav-collapse collapse">
 		   <ul class="nav">
-			  <li><a href="http://http://174.34.170.64/bootstrap.php/selection.php">Build Projects</a></li>
+			  <li><a href="selection.php">Build Projects</a></li>
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Saved Projects <b class="caret"></b></a>
                 <ul class="dropdown-menu">
