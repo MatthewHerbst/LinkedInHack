982c707bcc0517b5fc145dcd8473e2716c5caaf6
Ankit Agarwal <ankit.agarwal@ankagarwal-ltm.local>
2013-07-13 05:16:56 -0700

    test

--- bootstrap.php
+++ bootstrap.php
@@ -107,50 +107,6 @@ if(isset($_REQUEST['cmd'])) {
 </head>
 
 <body bgcolor="#EEEEEE">
- 				<!--   <div class="navbar navbar-inverse navbar-fixed-top">
-		<div class="navbar-inner">
-			<div class="container">
-				<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="brand" href="#">Wingman</a>
-				 <div class="nav-collapse collapse">
-					<ul class="nav">
-						<li class="active"><a href="#">Home</a></li>
-						<li><a href="#about">About</a></li>
-						<li><a href="#contact">Contact</a></li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown">My Packages <b class="caret"></b></a>
-							<ul class="dropdown-menu">
-								<li><a href="#">Action</a></li>
-								<li><a href="#">Another action</a></li>
-								<li><a href="#">Something else here</a></li>
-								<li class="divider"></li>
-								<li class="nav-header">Nav header</li>
-								<li><a href="#">Separated link</a></li>
-								<li><a href="#">One more separated link</a></li>
-							</ul>
-						</li>
-					</ul>
-					<form class="navbar-form pull-right" method='post' action='bootstrap.php'>
-						<?php //Show login/register options
-						if($user_pk == ""): ?> 
-							<input class="span2" type="text" name='username' maxlength="30" id='username' placeholder="Username">
-							<input class="span2" type="password" name='password' maxlength="30" id='password' placeholder="Password">
-							<input type="submit" class="btn" name='cmd' value='Login'>
-							<input type="submit" class="btn" name='cmd' value='Register'>
-						<?php //Show logout option
-						else: ?>
-							<button type="submit" class="btn">Logout</button>
-							<input type='hidden' name='cmd' value='logout' />
-						<?php endif; ?>
-					</form>
-				</div> 
-			</div>
-		</div>
-    </div> -->
 
     <div class="container">
 
